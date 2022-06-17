f6d3cb980265c02ca25fedc28a0ffa015bac6ff6
jaimefreyre <estudiobudapest@gmail.com>
2022-06-17 18:34:48 -0300

    viernes176

--- src/App.vue
+++ src/App.vue
@@ -10,20 +10,24 @@
         <!-- Right aligned nav items -->
         <b-navbar-nav class="ml-auto blanco_fondo">
           <b-navbar-nav right >
-            <b-nav-item href="#">Link</b-nav-item>
-            <b-nav-item href="#" disabled>Disabled</b-nav-item>
-            <b-nav-item href="#" disabled>Disabled</b-nav-item>
-            <b-nav-item href="#" disabled>Disabled</b-nav-item>
-            <b-nav-item href="#" disabled>Disabled</b-nav-item>
-            <b-nav-item href="#" disabled>Disabled</b-nav-item>
+            <b-nav-item style="margin-top: 10px;" href="#">CLIENTES</b-nav-item>
+            <b-nav-item style="margin-top: 10px;" href="#">PRECIOS</b-nav-item>
+            <b-nav-item style="margin-top: 10px;" href="#">NOSOTROS</b-nav-item>
+            <b-nav-item style="margin-top: 10px;" href="#">CONTACTOS</b-nav-item>
+            <b-nav-item>
+              <b-button variant="success">INGRESO</b-button>
+            </b-nav-item>
+            <b-nav-item>
+              <b-button variant="primary">DEMO GRATUITA</b-button>
+            </b-nav-item>
           </b-navbar-nav>
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
     
     <b-container class="bv-example-row azul_gradiente seccion" fluid>
-      <b-row class="Digitaliza" align-v="stretch">
-        <b-col>
+      <b-row class="Digitaliza" align-v="center">
+        <b-col cols="12" md="6">
           <h2>
             Digitaliza tus procesos <br>
             logisticos para lograr <br>          
@@ -37,9 +41,9 @@
 
 
     <b-container class="bv-example-row seccion2" fluid>
-      <b-row class="implementa" aling-v="stretch">
+      <b-row class="implementa" aling-v="center">
         <b-col>1 of 2</b-col>
-        <b-col>
+        <b-col cols="12" md="6">
           <h2>
             Implementa el mejor <br>
             sistema de gestion <br>
@@ -60,8 +64,8 @@
         </b-col>
       </b-row>
       <b-row>
-        <b-col aling-v="center" class="text-center">
-          <h2 style="color:blue">
+        <b-col aling-v="center" class="text-center" cols="12">
+          <h2 style="color:blue; margin-bottom: 30px;">
             CARDMIND permite a tu empresa <br>
             tener datos certeros y en un instante sobre:
           </h2>
@@ -70,59 +74,277 @@
     </b-container>  
 
     <b-container class="bv-example-row seccion3 dark" fluid>
-      <b-row>
-        <b-col>1 of 2</b-col>
-        <b-col>2 of 2</b-col>
+      <b-row style="padding: 5em;">
+        <b-col cols="12" md="4" aling-v="center" >
+          <b-list-group style="margin: 10px;">
+            <b-list-group-item href="#some-link">Awesome link</b-list-group-item>
+            <b-list-group-item href="#" active>Link with active state</b-list-group-item>
+            <b-list-group-item href="#">Action links are easy</b-list-group-item>
+            <b-list-group-item href="#foobar" disabled>Disabled link</b-list-group-item>
+          </b-list-group>
+        </b-col>
+        <b-col cols="12" md="4" aling-v="center" >
+          <b-list-group style="margin: 10px;">
+            <b-list-group-item href="#some-link">Awesome link</b-list-group-item>
+            <b-list-group-item href="#" active>Link with active state</b-list-group-item>
+            <b-list-group-item href="#">Action links are easy</b-list-group-item>
+            <b-list-group-item href="#foobar" disabled>Disabled link</b-list-group-item>
+          </b-list-group>
+        </b-col>
+        <b-col cols="12" md="4" aling-v="center" >
+          <b-list-group style="margin: 10px;">
+            <b-list-group-item href="#some-link">Awesome link</b-list-group-item>
+            <b-list-group-item href="#" active>Link with active state</b-list-group-item>
+            <b-list-group-item href="#">Action links are easy</b-list-group-item>
+            <b-list-group-item href="#foobar" disabled>Disabled link</b-list-group-item>
+          </b-list-group>
+        </b-col>
       </b-row>
     </b-container>  
 
 
     <b-container class="bv-example-row azul_gradiente seccion_corto" fluid>
       <b-row>
-        <b-col>1 of 2</b-col>
-        <b-col>2 of 2</b-col>
+        <b-col style="padding: 2em; color: white;">
+          <h3 >
+            La visibilidad en tiempo real es necesaria para tomar descisiones rapidas <br>
+            e inteligentes que te permitan superar las expectativas de tus clientes
+          </h3>
+          <br>
+          <div>
+            <b-button  variant="success">
+              Haciendo click aqui obtienes una demo gratuita
+            </b-button>
+          </div>
+        </b-col>
       </b-row>
     </b-container>  
 
     <b-container class="bv-example-row seccion2" fluid>
       <b-row>
-        <b-col>1 of 2</b-col>
-        <b-col>2 of 2</b-col>
+        <b-col cols="12" md="6">
+          <h3 style="padding-top: 2em;" class="text-center">
+            Obten el total control de tu flota con CarMind: <br>
+            El sistema intuitivo y facil de usar.
+          </h3>
+          <b-button variant="success">
+            Solicita tu demo gratuita aqui
+          </b-button>
+        </b-col>
+        <b-col cols="12" md="6">
+          <h2 style="padding-top: 2em;">
+            <span>Control total </span>en <br>
+            una sola herramienta <br>
+            completamente <br>
+            unificada 
+          </h2>
+          <b-list-group>
+            <b-list-group-item>Cras justo odio</b-list-group-item>
+            <b-list-group-item active>Dapibus ac facilisis in</b-list-group-item>
+            <b-list-group-item>Morbi leo risus</b-list-group-item>
+            <b-list-group-item>Porta ac consectetur ac</b-list-group-item>
+            <b-list-group-item>Vestibulum at eros</b-list-group-item>
+          </b-list-group>
+        </b-col>
       </b-row>
     </b-container>  
 
     <b-container class="bv-example-row seccion seccion_corto" fluid>
-      <b-row>
-        <b-col>1 of 2</b-col>
-        <b-col>2 of 2</b-col>
+      <b-row style="padding-top: 3em;">
+        <b-col cols="12">
+          <h4>EMPRESAS QUE YA ELIGIERON CARDMIND </h4>
+        </b-col>
+        <b-col cols="4">
+           <b-card
+            overlay
+            img-src="https://picsum.photos/900/250/?image=3"
+            img-alt="Card Image"
+            text-variant="white"
+            title="Image Overlay"
+            sub-title="Subtitle"
+          >
+            <b-card-text>
+              Some quick example text to build on the card and make up the bulk of the card's content.
+            </b-card-text>
+          </b-card>
+        </b-col>
+        <b-col cols="4">
+           <b-card
+            overlay
+            img-src="https://picsum.photos/900/250/?image=3"
+            img-alt="Card Image"
+            text-variant="white"
+            title="Image Overlay"
+            sub-title="Subtitle"
+          >
+            <b-card-text>
+              Some quick example text to build on the card and make up the bulk of the card's content.
+            </b-card-text>
+          </b-card>
+        </b-col>
+        <b-col cols="4">
+           <b-card
+            overlay
+            img-src="https://picsum.photos/900/250/?image=3"
+            img-alt="Card Image"
+            text-variant="white"
+            title="Image Overlay"
+            sub-title="Subtitle"
+          >
+            <b-card-text>
+              Some quick example text to build on the card and make up the bulk of the card's content.
+            </b-card-text>
+          </b-card>
+        </b-col>
       </b-row>
     </b-container>  
 
     <b-container class="bv-example-row seccion" fluid>
       <b-row>
-        <b-col>1 of 2</b-col>
-        <b-col>2 of 2</b-col>
+        <b-col cols="12" class="text-center" style="padding-top: 3em;">
+          <h2>¿Porque deberias <br> elegir CardMind?</h2>
+          <h6>---- obtienes estas ventajas competitivas ---</h6>
+        </b-col>
+        <b-col cols="4">
+          <b-card
+            overlay
+            img-src="https://picsum.photos/900/250/?image=3"
+            img-alt="Card Image"
+            text-variant="white"
+            title="Image Overlay"
+            sub-title="Subtitle"
+          >
+            <b-card-text>
+              Some quick example text to build on the card and make up the bulk of the card's content.
+            </b-card-text>
+          </b-card>
+        </b-col>
+        <b-col cols="4">
+          <b-card
+            overlay
+            img-src="https://picsum.photos/900/250/?image=3"
+            img-alt="Card Image"
+            text-variant="white"
+            title="Image Overlay"
+            sub-title="Subtitle"
+          >
+            <b-card-text>
+              Some quick example text to build on the card and make up the bulk of the card's content.
+            </b-card-text>
+          </b-card>
+        </b-col>
+        <b-col cols="4">
+          <b-card
+            overlay
+            img-src="https://picsum.photos/900/250/?image=3"
+            img-alt="Card Image"
+            text-variant="white"
+            title="Image Overlay"
+            sub-title="Subtitle"
+          >
+            <b-card-text>
+              Some quick example text to build on the card and make up the bulk of the card's content.
+            </b-card-text>
+          </b-card>
+        </b-col>
       </b-row>
     </b-container>  
 
     <b-container class="bv-example-row seccion fondo_imagen" fluid>
       <b-row>
-        <b-col>1 of 2</b-col>
-        <b-col>2 of 2</b-col>
+        <b-col style="margin:30px;">
+          <div>
+            <b-carousel
+              id="carousel-1"
+              v-model="slide"
+              :interval="4000"
+              controls
+              indicators
+              background="#ababab"
+              img-width="1024"
+              img-height="250"
+              style="text-shadow: 1px 1px 2px #333;"
+              @sliding-start="onSlideStart"
+              @sliding-end="onSlideEnd"
+            >
+              <!-- Text slides with image -->
+              <b-carousel-slide
+                caption="First slide"
+                text="Nulla vitae elit libero, a pharetra augue mollis interdum."
+                img-src="https://picsum.photos/1024/480/?image=52"
+              ></b-carousel-slide>
+
+              <!-- Slides with custom text -->
+              <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=54">
+                <h1>Hello world!</h1>
+              </b-carousel-slide>
+
+              <!-- Slides with image only -->
+              <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=58"></b-carousel-slide>
+
+              <!-- Slides with img slot -->
+              <!-- Note the classes .d-block and .img-fluid to prevent browser default image alignment -->
+              <b-carousel-slide>
+                <template #img>
+                  <img
+                    class="d-block img-fluid w-100"
+                    width="1024"
+                    height="250"
+                    src="https://picsum.photos/1024/480/?image=55"
+                    alt="image slot"
+                  >
+                </template>
+              </b-carousel-slide>
+
+              <!-- Slide with blank fluid image to maintain slide aspect ratio -->
+              <b-carousel-slide caption="Blank Image" img-blank img-alt="Blank image">
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eros felis, tincidunt
+                  a tincidunt eget, convallis vel est. Ut pellentesque ut lacus vel interdum.
+                </p>
+              </b-carousel-slide>
+            </b-carousel>
+          </div>
+        </b-col>
+        
       </b-row>
     </b-container>  
 
     <b-container class="bv-example-row azul_gradiente seccion_corto" fluid>
-      <b-row>
-        <b-col>1 of 2</b-col>
-        <b-col>2 of 2</b-col>
+      <b-row  style="padding:10vh  ;">
+        <b-col cols="12" >
+          <h2 class="text-center" style="color:whitesmoke">
+            Quiero gestionar mi flota de vehiculos.
+          </h2>
+        </b-col>
+        <b-col cols="12" >
+          <b-button variant="success">
+            SOLICITA TU DEMO GRATUITA AQUI
+          </b-button>
+        </b-col>
       </b-row>
     </b-container>  
 
     <b-container class="bv-example-row seccion dark" fluid>
-      <b-row>
-        <b-col>1 of 2</b-col>
-        <b-col>2 of 2</b-col>
+      <b-row style="padding:5em;">
+        <b-col>
+          <b-list-group class="dark">
+            <b-list-group-item class="dark">Cras justo odio</b-list-group-item>
+            <b-list-group-item class="dark">Dapibus ac facilisis in</b-list-group-item>
+            <b-list-group-item class="dark">Morbi leo risus</b-list-group-item>
+            <b-list-group-item class="dark">Porta ac consectetur ac</b-list-group-item>
+            <b-list-group-item class="dark">Vestibulum at eros</b-list-group-item>
+          </b-list-group>
+        </b-col>
+        <b-col>
+          <b-list-group class="dark">
+            <b-list-group-item class="dark">Cras justo odio</b-list-group-item>
+            <b-list-group-item class="dark">Dapibus ac facilisis in</b-list-group-item>
+            <b-list-group-item class="dark">Morbi leo risus</b-list-group-item>
+            <b-list-group-item class="dark">Porta ac consectetur ac</b-list-group-item>
+            <b-list-group-item class="dark">Vestibulum at eros</b-list-group-item>
+          </b-list-group>
+        </b-col>
       </b-row>
     </b-container>  
 
@@ -135,7 +357,17 @@ export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Your Vue.js App',
+      slide: 0,
+      sliding: null
+    }
+  },
+  methods: {
+    onSlideStart(slide) {
+      this.sliding = true
+    },
+    onSlideEnd(slide) {
+      this.sliding = false
     }
   }
 }
@@ -167,24 +399,27 @@ a {
 }
 
 .seccion_corto{
-  height: 28vh !important;
+  /*height: 28vh !important;*/
 }
 
 .seccion{
-  height: 45vh !important;
+  /*height: 45vh !important;*/
 }
 
 .seccion2{
-  height: 75vh !important;
+  /*height: 75vh !important;*/
 }
 
 .seccion3{
-  height: 50vh !important;
+  /*height: 50vh !important;*/
   background-color: #051937;
 }
 
 .dark{
-  background-color: #051937;
+  background-color: black;
+  color: white;
+  /*padding: 5em;
+  vertical-align:center;*/
 }
 
 .blanco_fondo{
